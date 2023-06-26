c6b0787a0e61c131555ded9fea334fb76008169b
Mario Carneiro <di.gama@gmail.com>
2023-06-26 08:08:37 -0400

    zstd dictionary compression

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ fn main() {
   let mut do_selftest = false;
   #[cfg(feature = "debug")]
   let mut do_stats = false;
+  #[cfg(feature = "zstd-train")]
+  let mut do_training = false;
   let mut verbose = false;
   let mut outfile = None;
   let mut args = std::env::args();
@@ -29,6 +31,11 @@ fn main() {
       #[cfg(feature = "debug")]
       "-t" => {
         do_selftest = true;
+        args.next();
+      }
+      #[cfg(feature = "zstd-train")]
+      "--train" => {
+        do_training = true;
         args.next();
       }
       #[cfg(feature = "debug")]
@@ -51,14 +58,16 @@ fn main() {
   }
   #[cfg(feature = "debug")]
   if do_selftest {
+    let compressor = Compressor::new();
+    let decompressor = Decompressor::new();
     let mut lgzs = 0;
     let mut oleans = 0;
     for file in args {
       println!("{file}");
       let mmap = unsafe { Mmap::map(&File::open(file).unwrap()).unwrap() };
       let mut lgzfile = vec![];
-      compress(&mmap, std::io::Cursor::new(&mut lgzfile));
-      let oleanfile = decompress(std::io::Cursor::new(&lgzfile));
+      compressor.compress(&mmap, std::io::Cursor::new(&mut lgzfile));
+      let oleanfile = decompressor.decompress(std::io::Cursor::new(&lgzfile));
       assert!(*mmap == *oleanfile);
       lgzs += lgzfile.len();
       oleans += oleanfile.len();
@@ -72,61 +81,136 @@ fn main() {
     println!("{} / {} = {:.6}", oleans, lgzs, oleans as f64 / lgzs as f64);
     return
   }
+  #[cfg(feature = "zstd-train")]
+  if do_training {
+    let mut samples = vec![];
+    let mut sizes = vec![];
+    let mut max_size = 0;
+    for file in args {
+      if verbose {
+        println!("{file}");
+      }
+      let start = samples.len();
+      let mmap = unsafe { Mmap::map(&File::open(file).unwrap()).unwrap() };
+      leangz::compress(&mmap, std::io::Cursor::new(&mut samples));
+      let size = samples.len() - start;
+      sizes.push(size);
+      max_size = max_size.max(size);
+    }
+    println!("starting training");
+    let outfile = outfile.take().unwrap_or_else(|| format!("src/lgz.dict"));
+    let dict = zstd::dict::from_continuous(&samples, &sizes, max_size).unwrap();
+    std::fs::write(outfile, dict).unwrap();
+    return
+  }
   #[cfg(feature = "debug")]
   if do_stats {
+    let compressor = Compressor::new();
     let mut lgzs = 0;
     let mut oleans = 0;
     for file in args {
       let mmap = unsafe { Mmap::map(&File::open(file).unwrap()).unwrap() };
       let mut out = leangz::WithPosition { r: std::io::sink(), pos: 0 };
-      compress(&mmap, &mut out);
-      lgzs += mmap.len();
-      oleans += out.pos;
+      compressor.compress(&mmap, &mut out);
+      oleans += mmap.len();
+      lgzs += out.pos;
     }
     for (i, (n, e)) in STATS.normal.iter().zip(&STATS.exprish).enumerate() {
       println!("{i:x}: normal {} exprish {}", n.load(Ordering::Relaxed), e.load(Ordering::Relaxed));
     }
-    println!("{} / {} = {:.6}", lgzs, oleans, lgzs as f64 / oleans as f64);
+    println!("{} / {} = {:.6}", oleans, lgzs, oleans as f64 / lgzs as f64);
     return
   }
   if do_decompress {
+    let decompressor = Decompressor::new();
     for file in args {
       if verbose {
         println!("{file}");
       }
       let outfile = outfile.take().unwrap_or_else(|| format!("{file}.olean"));
       let file = File::open(file).unwrap();
-      std::fs::write(outfile, decompress(file)).unwrap()
+      std::fs::write(outfile, decompressor.decompress(file)).unwrap()
     }
   } else {
+    let compressor = Compressor::new();
+    let mut lgzs = 0;
+    let mut oleans = 0;
     for file in args {
       if verbose {
         println!("{file}");
       }
       let outfile = outfile.take().unwrap_or_else(|| format!("{file}.lgz"));
       let mmap = unsafe { Mmap::map(&File::open(file).unwrap()).unwrap() };
-      compress(&mmap, BufWriter::new(File::create(outfile).unwrap()));
+      oleans += mmap.len();
+      let outfile = BufWriter::new(File::create(outfile).unwrap());
+      let mut w = leangz::WithPosition { r: outfile, pos: 0 };
+      compressor.compress(&mmap, &mut w);
+      lgzs += w.pos;
     }
+    println!("{} / {} = {:.6}", oleans, lgzs, oleans as f64 / lgzs as f64);
   }
 }
 
-fn compress(olean: &[u8], outfile: impl Write) {
-  #[cfg(feature = "flate2")]
-  let outfile = flate2::write::GzEncoder::new(outfile, flate2::Compression::new(7));
-  #[cfg(feature = "zstd")]
-  let outfile = zstd::stream::Encoder::new(outfile, 19).unwrap();
-  let mut outfile = outfile;
-  leangz::compress(olean, &mut outfile);
-  #[cfg(any(feature = "flate2", feature = "zstd"))]
-  outfile.finish().unwrap();
-  #[cfg(not(any(feature = "flate2", feature = "zstd")))]
-  outfile.flush().unwrap();
+#[cfg(feature = "zstd")]
+const COMPRESSION_LEVEL: i32 = 19;
+
+struct Compressor<'a> {
+  #[cfg(feature = "zstd-dict")]
+  dict: zstd::dict::EncoderDictionary<'a>,
+  _phantom: &'a (),
 }
 
-fn decompress(infile: impl Read) -> Vec<u8> {
-  #[cfg(feature = "flate2")]
-  let infile = flate2::read::GzDecoder::new(infile);
-  #[cfg(feature = "zstd")]
-  let infile = zstd::stream::Decoder::new(infile).unwrap();
-  leangz::decompress(infile)
+#[cfg(feature = "zstd-dict")]
+const ZSTD_DICT: &[u8] = include_bytes!("lgz.dict");
+
+impl Compressor<'_> {
+  fn new() -> Self {
+    Compressor {
+      #[cfg(feature = "zstd-dict")]
+      dict: zstd::dict::EncoderDictionary::copy(ZSTD_DICT, COMPRESSION_LEVEL),
+      _phantom: &(),
+    }
+  }
+
+  fn compress(&self, olean: &[u8], outfile: impl Write) {
+    #[cfg(feature = "flate2")]
+    let outfile = flate2::write::GzEncoder::new(outfile, flate2::Compression::new(7));
+    #[cfg(all(feature = "zstd", not(feature = "zstd-dict")))]
+    let outfile = zstd::stream::Encoder::new(outfile, COMPRESSION_LEVEL).unwrap();
+    #[cfg(feature = "zstd-dict")]
+    let outfile = zstd::stream::Encoder::with_prepared_dictionary(outfile, &self.dict).unwrap();
+    let mut outfile = outfile;
+    leangz::compress(olean, &mut outfile);
+    #[cfg(any(feature = "flate2", feature = "zstd"))]
+    outfile.finish().unwrap();
+    #[cfg(not(any(feature = "flate2", feature = "zstd")))]
+    outfile.flush().unwrap();
+  }
+}
+
+struct Decompressor<'a> {
+  #[cfg(feature = "zstd-dict")]
+  dict: zstd::dict::DecoderDictionary<'a>,
+  _phantom: &'a (),
+}
+
+impl Decompressor<'_> {
+  fn new() -> Self {
+    Decompressor {
+      #[cfg(feature = "zstd-dict")]
+      dict: zstd::dict::DecoderDictionary::copy(ZSTD_DICT),
+      _phantom: &(),
+    }
+  }
+  fn decompress(&self, infile: impl Read) -> Vec<u8> {
+    #[cfg(feature = "flate2")]
+    let infile = flate2::read::GzDecoder::new(infile);
+    #[cfg(all(feature = "zstd", not(feature = "zstd-dict")))]
+    let infile = zstd::stream::Decoder::new(infile).unwrap();
+    #[cfg(feature = "zstd-dict")]
+    let infile =
+      zstd::stream::Decoder::with_prepared_dictionary(std::io::BufReader::new(infile), &self.dict)
+        .unwrap();
+    leangz::decompress(infile)
+  }
 }
